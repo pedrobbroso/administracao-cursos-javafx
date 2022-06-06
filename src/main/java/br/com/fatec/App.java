d500ce1163d912da8beaa4c694efc6abd70ee956
Pedro <pedro.broso@gmail.com>
2022-06-06 12:18:09 -0300

    Criando about

--- src/main/java/br/com/fatec/App.java
+++ src/main/java/br/com/fatec/App.java
@@ -30,6 +30,10 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
+    
+    public static Scene getScene() {
+        return scene;
+    }
 
     public static void main(String[] args) {
         launch();
--- src/main/java/br/com/fatec/Controller/MainViewController.java
+++ src/main/java/br/com/fatec/Controller/MainViewController.java
@@ -4,11 +4,20 @@
  */
 package br.com.fatec.controller;
 
+import br.com.fatec.App;
+import gui.util.Alerts;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -19,39 +28,53 @@ public class MainViewController implements Initializable {
 
     @FXML
     private MenuItem menuItemCurso;
-    
+
     @FXML
     private MenuItem menuItemInstrutor;
-    
+
     @FXML
     private MenuItem menuItemTurma;
-    
+
     @FXML
     private MenuItem menuItemAbout;
-    
+
     public void onMenuItemCursoAction() {
         System.out.println("onMenuItemCursoAction");
     }
-    
+
     public void onMenuItemInstrutorAction() {
         System.out.println("onMenuItemInstrutorAction");
     }
-    
+
     public void onMenuItemTurmaAction() {
         System.out.println("onMenuItemTurmaAction");
     }
-    
+
     public void onMenuItemAboutAction() {
-        System.out.println("onMenuItemAboutAction");
+        carregaView("about");
     }
 
-    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    private void carregaView(String nomeAbsoluto) {
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource(nomeAbsoluto + ".fxml"));
+            VBox newVbox = loader.load();
+            Scene scene = App.getScene();
+            VBox mainVBox = (VBox) ((ScrollPane) scene.getRoot()).getContent();
+            
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVbox.getChildren());
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error ao carregar a view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
 }
